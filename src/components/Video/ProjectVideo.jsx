a8c50052a5306253b8c7af763dedbab8ecb3ebac
Daniel Chu <dannyychuu@gmail.com>
2021-02-10 22:10:23 -0800

    fixed mvideo mute attribute in DOM

--- src/components/Video/ProjectVideo.jsx
+++ src/components/Video/ProjectVideo.jsx
@@ -3,13 +3,26 @@ import { StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 
 const ProjectVideo = ({ filename, alt }) => (
-  <video className="embed-responsive" loop autoPlay muted playsInline>
-    <source
-      className="embed-responsive-item"
-      src={require(`../../videos/${filename}`)}
-      type="video/mp4"
-    />
-  </video>
+  // <video className="embed-responsive" loop autoPlay playsInline muted>
+  //   <source
+  //     className="embed-responsive-item"
+  //     src={require(`../../videos/${filename}`)}
+  //     type="video/mp4"
+  //   />
+  // </video>
+  <div
+    dangerouslySetInnerHTML={{
+      __html: `
+<video class="embed-responsive" loop autoPlay playsInline muted>
+<source
+  class="embed-responsive-item"
+  src=${require(`../../videos/${filename}`)}
+  type="video/mp4"
+/>
+</video>
+`,
+    }}
+  />
 );
 
 ProjectVideo.propTypes = {
